af1c87a7d72e0014888988d529760aa408f99be5
Mark Briscoe <mark@mcmw.co.uk>
2025-12-18 11:33:46 +0000

    update: environment variables setup

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_@zmsnha53bw*!4q%hh9t0zqw^9h4_)jf%(r!lqlpu*e*_7j*'
+if os.path.isfile("env.py"):
+    import env
+    DEBUG = True
+else:
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
 
 ALLOWED_HOSTS = []
 
